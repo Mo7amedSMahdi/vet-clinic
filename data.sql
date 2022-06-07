6305aa56656867ef186862d5f90e13bd981e706b
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-06-07 22:44:08 +0300

    Vet clinic database: query and update animals table

--- data.sql
+++ data.sql
@@ -1,9 +1,15 @@
 /* Populate database with sample data. */
 
 INSERT INTO "public"."animals" ("id", "name", "date_of_birth", "escape_attempts", "neutered", "weight_kg") VALUES (1, 'Agumon', '2020-3-2', 0, 't', 10.23);
-
 INSERT INTO "public"."animals" ("id", "name", "date_of_birth", "escape_attempts", "neutered", "weight_kg") VALUES (2, 'Gabumon', '2018-11-15', 2, 't', 8);
-
 INSERT INTO "public"."animals" ("id", "name", "date_of_birth", "escape_attempts", "neutered", "weight_kg") VALUES (3, 'Pikachu', '2021-7-1', 1, 'f', 15.04);
-
 INSERT INTO "public"."animals" ("id", "name", "date_of_birth", "escape_attempts", "neutered", "weight_kg") VALUES (4, 'Devimon', '2017-12-5', 5, 't', 11);
+
+
+INSERT INTO "public"."animals" ("id", "name", "date_of_birth", "escape_attempts", "neutered", "weight_kg") VALUES (5, 'Charmander', '2020-2-8', 0, FALSE, -11.00);
+INSERT INTO "public"."animals" ("id", "name", "date_of_birth", "escape_attempts", "neutered", "weight_kg") VALUES (6, 'Plantmon', '2021-11-15', 2, TRUE, -5.70);
+INSERT INTO "public"."animals" ("id", "name", "date_of_birth", "escape_attempts", "neutered", "weight_kg") VALUES (7, 'Squirtle', '1993-4-2', 3, FALSE, -12.13);
+INSERT INTO "public"."animals" ("id", "name", "date_of_birth", "escape_attempts", "neutered", "weight_kg") VALUES (8, 'Angemon', '2005-6-12', 1, TRUE, -45);
+INSERT INTO "public"."animals" ("id", "name", "date_of_birth", "escape_attempts", "neutered", "weight_kg") VALUES (9, 'Boarmon', '2005-6-7', 7, TRUE, 20.40);
+INSERT INTO "public"."animals" ("id", "name", "date_of_birth", "escape_attempts", "neutered", "weight_kg") VALUES (10, 'Blossom', '1998-10-13', 3, TRUE, 17.00);
+INSERT INTO "public"."animals" ("id", "name", "date_of_birth", "escape_attempts", "neutered", "weight_kg") VALUES (11, 'Ditto', '2022-5-14', 4, TRUE, 22.00);
