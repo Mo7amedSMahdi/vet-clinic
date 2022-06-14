07bfe4f11c739ff6a7ffdeae00e69b50a5b97da8
Alfred <kalumunabisimwa5@gmail.com>
2022-06-14 11:04:19 +0200

    3rd_query_before_and_after

--- queries.sql
+++ queries.sql
@@ -215,4 +215,6 @@ LIMIT 1;
 
 -- The following queries are taking too much time 
 -- (1 sec = 1000ms can be considered as too much time for database query). Try them on your machine to confirm it:
-explain analyze SELECT COUNT(*) FROM visits where animal_id = 4
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+explain analyze SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
